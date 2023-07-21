97dd3bc076ba3ddc16a1bfae111349155e0a87ce
mjlover05 <93177731+mjlover05@users.noreply.github.com>
2023-07-21 11:53:48 +0530

    Update fun_error.sol

--- fun_error.sol
+++ fun_error.sol
@@ -1,22 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-contract errorDec {
-    function Assert(uint a) public pure{
-    assert(a!= 0);
-    
-}
-function Require(uint b) public pure{
 
-        require( b>5, "Input must be greater than 5");
-        }
-    
-
-function Revert(uint a,uint b) public pure returns(uint) {
-    if( a<b){
-        revert("a should be greater");
+contract ErrorFun {
+    function Mod_error(uint a, uint b) public pure returns (uint) {
+        uint result = a % b;
+        assert(result < b); 
+        return result;
     }
-    return a-b;
 
-}
 
+    function quotient_even(uint a) public pure returns (uint) {
+        if (a%2!=0) {
+            revert("its a odd number");
+        }
+        return a/2;
+    }
+
+    
+
+    function Zero_div_error(uint num, uint dem) public pure returns (uint) {
+        require(dem != 0, "Cannot divide by zero");
+        uint result = num / dem;
+        return result;
+    }
 }
